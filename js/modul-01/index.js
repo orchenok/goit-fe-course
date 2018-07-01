eca16db83afcf01477a38a582cb4a2e5c7816c6f
orchenok <orchenok@gmail.com>
2018-07-02 02:42:31 +0300

    add modul 01

--- js/modul-01/index.js
+++ js/modul-01/index.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const ADMIN_LOGIN  =  'admin' ;
+const ADMIN_PASSWORD  =  'm4ngo1zh4ackz0r' ;
+
+const userCancel = 'Отменено пользователем!';
+const userError = 'Доступ запрещен!';
+const userCorrect = 'Добро пожаловать!';
+
+const userLogin = prompt('log in');
+let userPass;
+
+if (userLogin === null) {
+  alert(userCancel);
+} else if (userLogin === ADMIN_LOGIN) {
+  userPass = prompt('Enter yuor password');
+  
+  switch (userPass) {
+    case null: alert(userCancel);
+    break;
+    case 'm4ngo1zh4ackz0r': alert(userCorrect);
+    break;
+    default: alert(userError);     
+  }  
+} else {
+  alert(userError);
+}
